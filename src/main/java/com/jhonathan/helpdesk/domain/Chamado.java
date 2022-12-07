73fe2c9b16e3dee5517c61eef5215a2b4ef3916b
jhonathan Cruz <jhonathancruz48@gmail.com>
2022-12-07 19:51:23 -0300

    anotações JPA para classes de dominio

--- src/main/java/com/jhonathan/helpdesk/domain/Chamado.java
+++ src/main/java/com/jhonathan/helpdesk/domain/Chamado.java
@@ -1,22 +1,49 @@
 package com.jhonathan.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jhonathan.helpdesk.domain.enuns.Prioridade;
 import com.jhonathan.helpdesk.domain.enuns.Status;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable{
 
+	private static final long serialVersionUID = 2346206349807228661L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer Id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	
 	private Prioridade prioridade;
+	
 	private Status status;
+	
 	private String titulo;
+	
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
 	public Chamado() {
--- src/main/java/com/jhonathan/helpdesk/domain/Tecnico.java
+++ src/main/java/com/jhonathan/helpdesk/domain/Tecnico.java
@@ -3,16 +3,28 @@ package com.jhonathan.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jhonathan.helpdesk.domain.enuns.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa{
 	
+	
+	private static final long serialVersionUID = 214767078518795860L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public List<Chamado> getChamados() {
